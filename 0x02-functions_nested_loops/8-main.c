ec476185c0229919545418acec72bc56153b285a
Mariamugo <shee.mw70@gmail.com>
2023-09-13 17:20:59 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+void jack_bauer(void);
+        return (0);
+}
+
