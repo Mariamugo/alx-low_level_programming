92bee67633cfefd4ce65149492fedc5b1cad4183
Mariamugo <shee.mw70@gmail.com>
2023-09-13 19:44:02 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program prints alphabet in lowercae
+ * program written by Maria
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
