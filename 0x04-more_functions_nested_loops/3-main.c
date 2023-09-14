b8942f2ddffa08097a923f1f5a1d48eb6379acc7
Mariamugo <shee.mw70@gmail.com>
2023-09-14 14:43:42 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - declares function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+void print_numbers(void);
+        return (0);
+}
