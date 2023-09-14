c0fc1ef48637eac8080874d123dca5cbf55b0885
Mariamugo <shee.mw70@gmail.com>
2023-09-13 17:39:22 -0700

    task 11

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+void print_times_table(int n);
+        return (0);
+}
