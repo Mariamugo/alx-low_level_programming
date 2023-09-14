5007d440b6facfe96904faec9d19f1e976afb3f1
Mariamugo <shee.mw70@gmail.com>
2023-09-13 17:24:22 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+void times_table(void);
+        return (0);
+}
