3bcbc1afe2796e18033aaac41a33d4ccd3e12359
Mariamugo <shee.mw70@gmail.com>
2023-09-14 19:17:05 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int main (void);
+void positive_or_negative (int i);
+#endif
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - declares function
+ *
+ * Return: On sucess 1
+ */
+int main(void)
+{
+	positive_or_negative(int i);
+	return (0);
+
+}
