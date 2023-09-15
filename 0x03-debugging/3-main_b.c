5de92f20bc21b5d47ad0971dfb0078db5242ae44
Mariamugo <shee.mw70@gmail.com>
2023-09-14 19:29:52 -0700

    task 3

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - declares function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+void print_remaining_days(int month, int day, int year);
+	return (0);
+
+}
