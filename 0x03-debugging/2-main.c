5585565e3c340517f5fcd9453db99d67ae5b472e
Mariamugo <shee.mw70@gmail.com>
2023-09-14 19:23:51 -0700

    task 2

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On success 1
+ * On error, -1 is returned
+ */
+int largest_number(int a, int b, int c)
+{
+        return (0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,4 +3,5 @@
 int main (void);
 void positive_or_negative (int i);
 int _putchar(char c);
+int largest_number(int a, int b, int c);
 #endif
