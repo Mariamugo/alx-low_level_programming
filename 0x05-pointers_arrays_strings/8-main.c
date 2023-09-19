33d9c8bd2c7c89e51c8ee633ddbf80a0a3b685a4
Mariamugo <shee.mw70@gmail.com>
2023-09-18 19:42:40 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - declares function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+void print_array(int *a, int n);
+	return (0);
+
+}
