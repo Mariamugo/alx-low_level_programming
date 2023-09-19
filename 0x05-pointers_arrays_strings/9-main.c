efaf4a43994b8f4e702eefb1bd46b6e258f5d1e5
Mariamugo <shee.mw70@gmail.com>
2023-09-18 19:50:13 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On success 1
+ * On error, -1 is returned
+ */
+char *_strcpy(char *dest, char *src)
+{
+        return (0);
+}
