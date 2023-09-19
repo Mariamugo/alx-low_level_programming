e617974cb13f792746022527ce6a3b51b786432a
Mariamugo <shee.mw70@gmail.com>
2023-09-18 19:16:47 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - declares function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+	void _puts(char *str);
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
