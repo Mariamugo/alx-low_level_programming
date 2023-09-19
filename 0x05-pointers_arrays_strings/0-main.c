01a5d98e1fa044190382f1a4b7d1dae60a383e03
Mariamugo <shee.mw70@gmail.com>
2023-09-18 17:27:49 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - declares function
+ *
+ * Return: On sucess 1
+ * On error, -1 is returned
+ */
+int main(void)
+{
+	void reset_to_98(int *n);
+	return (0);
+
+}
+
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
