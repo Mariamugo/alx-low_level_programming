31ac52abb11d28b31855831e7aa897cbdb5d469c
Mariamugo <shee.mw70@gmail.com>
2023-09-22 17:26:59 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - declares _putchar function
+ *
+ * Return: On success 1
+ * On error, -1 is returned
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        return (0);
+}
