e24e6ac1a5e529e41df7c3085712e5f5ff4d6276
Mariamugo <shee.mw70@gmail.com>
2023-09-28 19:44:26 -0700

    task 8-compare2strings

---
+++ 0x08-recursion/101-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ *@s1
+ *@s2
+ *
+ */
+int wildcmp(char *s1, char *s2)
+{
+        return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,6 @@ int is_prime_number(int n);
 int actual_prime(int n, int i);
 int is_palindrome(char *s);
 int check_pal(char *s, int i, int len);
+int wildcmp(char *s1, char *s2);
 
 #endif
