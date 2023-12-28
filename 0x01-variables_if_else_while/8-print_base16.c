49e05f65bf8c3722fa391a1c9e6c7480c6b174f6
Mariamugo <shee.mw70@gmail.com>
2023-12-27 18:41:36 -0800

    prints base16 char

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - function prints all hexadecimal characters
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int X = 47;
+	int x = 'a';
+
+	while (X <= 57)
+	{
+		putchar(X);
+		X++;
+	}
+	while (x <= 'f')
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
