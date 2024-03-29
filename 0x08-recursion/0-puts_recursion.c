a32fd89f3099dd2ca8ca7b84d817d05ec8544ff4
Mariamugo <shee.mw70@gmail.com>
2024-03-28 22:55:09 -0700

    func traverse a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,28 +1,21 @@
-#include <stdio.h>
+#include <main.h>
 /*
  * this program prints a string followed by a new line
  * void _puts_recursion(char *s) - the function prototype
  * @*s - argument taken by the function, equals 1 byte
  * return void
  */
-/*declaring function prototype*/
-void _puts_recursion(char *s);
 
-int main()
-{
-	_putchar(*s);
-	/*calling the function*/
-	_puts_recursion(++s);
-
-	return;
-}
-/*defining the function*/
 void _puts_recursion(char *s)
 {
 	/*the basecase/terminating statement*/
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
+	}
+	else
+	{
+		_putchar(*s);
+		s = s + 1;
 	}
 }
