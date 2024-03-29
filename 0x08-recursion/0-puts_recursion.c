07fbe4e42a326ad6a4e484dc1668a4369f8c73a3
Mariamugo <shee.mw70@gmail.com>
2024-03-28 23:10:57 -0700

    func traverses string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- * this program prints a string followed by a new line
- * void _puts_recursion(char *s) - the function prototype
+ * this function prints a string followed by a new line
+ * _puts_recursion - the function prototype
  * @*s - argument taken by the function, equals 1 byte
  * return void
  */
