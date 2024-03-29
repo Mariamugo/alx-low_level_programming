6a8f55e64bc82dde8be9153d4f3703e1e47c7e2a
Mariamugo <shee.mw70@gmail.com>
2024-03-29 00:09:49 -0700

    print reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion - prints string in reverse
- * @*s: pointer argument
+ * @s: pointer argument
  * Return void
  */
 void _print_rev_recursion(char *s)
@@ -10,7 +10,7 @@ void _print_rev_recursion(char *s)
 	{
 		return;
 	}
-	else 
+	else
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
