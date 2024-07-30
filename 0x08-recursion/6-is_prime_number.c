8ca4b58c3eb8ad666258af61b457e5b25092646b
Mariamugo <shee.mw70@gmail.com>
2024-07-29 19:43:15 -0700

    checks_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,7 @@
 /**
  * is_prime_number - returns 1 if the integer is a prime number
  * @n: integer argument passed to the function
+ * @i: integer argument passed to function
  * Return: 0
  */
 int check_prime_number(int n, int i);
@@ -20,15 +21,15 @@ int check_prime_number(int n, int i)
 {
 	if (n <= 1)
 	{
-		return 0;
+		return (0);
 	}
 	if (n % i == 0 && i > 1)
 	{
-		return 0;
+		return (0);
 	}
 	if ((n / i) < i)
 	{
-		return 1;
+		return (1);
 	}
 
 	return (check_prime_number(n, i + 1));
